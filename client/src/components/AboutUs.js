3acb42a37b50688a79dbbb1f656e27e90e89d2be
Rakshajain13 <raksha.jain@technoidentity.com>
2023-06-04 19:36:52 +0530

    made many changes

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -4,7 +4,19 @@ const AboutUs = () => {
   return (
     <div>
       <NavBar />
-      <div> About Me</div>
+      <h1 className="font-bold text-7xl text-center">
+        FAST FOOD, FAST DELIVERY
+      </h1>
+      <h3 className="font-bold text-3xl">WHO ARE WE?</h3>
+      <div>
+        One year back, when two people join the same company and later became
+        good friends. They started working together,chit chatting, sharing their
+        feeling. They wanted to progress in life together,help each other so
+        wanted to build a project as their first step to success. We started
+        woring on this new food delivery app which will help us understand more
+        about MERN technology specially Mongodb and Express, and may be node.js
+        too.
+      </div>
     </div>
   );
 };
--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -9,16 +9,22 @@ const UserDashboard = () => {
       <NavBar />
       <div className="App  justify-center items-center ">
         <header className="App-header">
-          <div className="text-center text-white">
-            <p className=" mt-24  text-lg text-blue-400 ">
-              Welcome to Home page
-            </p>
-            <a href="/login">Login</a> <br />
-            <a href="/signup">Sign Up</a> <br />
-            <a href="/aboutus">About Us</a> <br />
-            <a href="/contactus">Contact Us</a> <br />
-          </div>
+          <div className="text-center text-white"></div>
         </header>
+        <footer className="App-footer ">
+          <div className="flex flex-nowrap">
+            <div className="pl-40 ">
+              <h1 className="text-black font-bold underline pb-4 ">Company</h1>
+              <a href="/aboutus">About Us</a>
+              <h1>Team</h1>
+              <h1>Careers</h1>
+            </div>
+            <div className="pl-40">
+              <h1 className="text-black font-bold underline pb-4 ">Contact</h1>
+              <h1>Help & Support</h1>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   );
