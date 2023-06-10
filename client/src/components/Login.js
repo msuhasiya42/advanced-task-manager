be1f8520970fbf2f0b96f0bc051470ccaf8f79e9
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-06-11 00:54:16 +0530

    Added err msg in login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [showErrMsg, setShowErrMsg] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -24,6 +24,7 @@ const Login = () => {
       // Perform any additional actions upon successful sign-up
     } catch (error) {
       console.error("Login error:", error);
+      setShowErrMsg(true);
       // Handle sign-up error
     }
   };
@@ -44,6 +45,30 @@ const Login = () => {
             Log In
           </a>
           <form className="bg-white shadow-lg border rounded px-8 pt-6 pb-8 mb-4">
+            {showErrMsg === true ? (
+              <div
+                class="flex p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+                role="alert"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="flex-shrink-0 inline w-5 h-5 mr-3"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>Invalid Credentials!</div>
+              </div>
+            ) : (
+              ""
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -104,7 +129,7 @@ const Login = () => {
             </p>
           </form>
           <p className="text-center text-gray-500 text-xs">
-            &copy;2023 Mayur Corp. All rights reserved.
+            &copy;2023 M & R Corp. All rights reserved.
           </p>
         </div>
       </div>
