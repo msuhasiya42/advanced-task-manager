60155bae78aaacb24c68a14e6a464e9c9bde8e11
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-06-11 00:59:38 +0530

    checking which repo gets update

--- client/src/components/ForgotPassword.js
+++ client/src/components/ForgotPassword.js
@@ -22,7 +22,7 @@ const ForgotPassword = () => {
             </h1>
             <p className="font-light text-gray-500 dark:text-gray-400">
               Don't fret! Just type in your email and we will send you a code to
-              reset your password!
+              reset your password.
             </p>
             <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
               <div>
