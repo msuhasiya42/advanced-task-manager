cb3e9fbb642a9a70829e30052277e2b39f511ce6
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-06-17 23:36:19 +0530

    added filter button and hided search button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -11,6 +11,8 @@ const Header = () => {
   // };
   const [user, setUser] = useState(null);
   const userId = localStorage.getItem("userId");
+  const categories = ["Apple", "Samsung", "Lenovo", "Nothing", "OnePlus"];
+  const tags = ["work", "personal", "jobhunt", "learning"];
 
   // for dark light theme
   // var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
@@ -125,7 +127,7 @@ const Header = () => {
             id="navbar-dropdown"
           >
             <ul className="flex flex-col font-medium p-2 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
+              {/* <li>
                 <form>
                   <label
                     htmlFor="default-search"
@@ -166,6 +168,123 @@ const Header = () => {
                     </button>
                   </div>
                 </form>
+              </li> */}
+
+              {/* filter button */}
+              <li>
+                <div class="flex items-center justify-center p-2">
+                  <button
+                    id="dropdownDefault"
+                    data-dropdown-toggle="dropdown"
+                    class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    type="button"
+                  >
+                    Filter
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      aria-hidden="true"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M19 9l-7 7-7-7"
+                      ></path>
+                    </svg>
+                  </button>
+
+                  {/* <!-- Dropdown menu --> */}
+                  <div
+                    id="dropdown"
+                    class="z-10 hidden w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700"
+                  >
+                    <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                      Members
+                    </h6>
+                    <ul
+                      class="space-y-2 text-sm mb-3"
+                      aria-labelledby="dropdownDefault"
+                    >
+                      {categories.map((category, index) => (
+                        <li key={index} class="flex items-center">
+                          <input
+                            id="apple"
+                            type="checkbox"
+                            value=""
+                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                          />
+
+                          <label
+                            for="apple"
+                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
+                          >
+                            {category}
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+
+                    {/* filter: by due date */}
+                    <hr />
+                    <h6 class="mb-2 mt-1 text-sm font-medium text-gray-900 dark:text-white">
+                      Due Date
+                    </h6>
+                    <ul
+                      class="space-y-2 text-sm mb-4"
+                      aria-labelledby="dropdownDefault"
+                    >
+                      {categories.map((category, index) => (
+                        <li key={index} class="flex items-center">
+                          <input
+                            id="apple"
+                            type="checkbox"
+                            value=""
+                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                          />
+
+                          <label
+                            for="apple"
+                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
+                          >
+                            {category}
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+
+                    {/* filter : tags */}
+                    <hr />
+                    <h6 class="mt-1 mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Tags
+                    </h6>
+                    <ul
+                      class="space-y-2 text-sm"
+                      aria-labelledby="dropdownDefault"
+                    >
+                      {tags.map((tag, index) => (
+                        <li key={index} class="flex items-center">
+                          <input
+                            id="apple"
+                            type="checkbox"
+                            value=""
+                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                          />
+
+                          <label
+                            for="apple"
+                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
+                          >
+                            {tag}
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
               </li>
               {/* user photo with options on it */}
 
@@ -229,7 +348,7 @@ const Header = () => {
                 >
                   <button
                     type="button"
-                    className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   >
                     Logout
                   </button>
@@ -237,6 +356,7 @@ const Header = () => {
               </li>
             </ul>
           </div>
+
           {/* nav bar right side buttons end */}
         </div>
       </nav>
