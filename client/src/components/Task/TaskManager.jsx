a957be8139fcbf2b7c47ecb39008b7d68f5401e1
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-06-20 12:40:41 +0530

    added loaging while task fetch

--- client/src/components/Task/TaskManager.jsx
+++ client/src/components/Task/TaskManager.jsx
@@ -3,6 +3,7 @@ import TaskList from "./TaskList";
 import { fetchTask } from "../../ApiCalls";
 import { updateTask } from "../../ApiCalls";
 import TaskAreaModal from "./TextAreaModal";
+import LoadingPage from "../Loading/LoadingPage";
 // import { todos, inprogress, completed } from "../../utils/data/static";
 
 const TaskManager = () => {
@@ -10,15 +11,18 @@ const TaskManager = () => {
   const [todos, setTodos] = useState([]);
   const [inprogress, setInprogress] = useState([]);
   const [completed, setCompleted] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const user = localStorage.getItem("userId");
 
   useEffect(() => {
     fetchTaskFun(user);
+    setLoading(false);
   }, [user]);
 
   // Fetch Task function
   const fetchTaskFun = async (user) => {
+    setLoading(true);
     fetchTask(user)
       .then((response) => {
         const fetchedTasks = response.data.tasks;
@@ -67,71 +71,78 @@ const TaskManager = () => {
 
   return (
     <div>
-      {/* task cards */}
       <div className="p-4  sm:ml-64 ">
         <div className="p-4  border-gray-800 border-dashed rounded-lg ">
-          <div className="grid grid-cols-3 gap-12 mb-4">
-            {/* to do card */}
-            <div className=" bg-gray-900 flex-col items-center  rounded ">
-              <div className="m-3">
-                <p className="m-2 text-center text-lg mt-1 text-white ">
-                  To-do
-                </p>
-                {/* task modal */}
-                <div>
-                  <TaskAreaModal status={"Todo"} addTask={addTask} />
-                  <TaskList
-                    tasks={todos}
-                    status={"Todo"}
-                    updateTaskFun={updateTaskFun}
-                  />
-                </div>
-                {/* task modal end */}
-              </div>
-            </div>
-            {/* to do card end */}
+          {loading ? (
+            <LoadingPage />
+          ) : (
+            <div className="grid grid-cols-3 gap-12 mb-4">
+              {/* to do card */}
 
-            {/* in-progress card */}
-            <div className=" bg-gray-900 flex-col items-center  rounded ">
-              <div className="m-3">
-                <p className="m-2 text-center text-lg mt-1 text-white ">
-                  In-Progress
-                </p>
-                <div>
-                  <TaskAreaModal status={"In Progress"} addTask={addTask} />
-                  <TaskList
-                    tasks={inprogress}
-                    status={"In Progress"}
-                    updateTaskFun={updateTaskFun}
-                  />
+              <div className=" bg-gray-900 flex-col items-center  rounded ">
+                <div className="m-3">
+                  <p className="m-2 text-center text-lg mt-1 text-white ">
+                    To-do
+                  </p>
+                  {/* task modal */}
+                  <div>
+                    <TaskAreaModal status={"Todo"} addTask={addTask} />
+                    <TaskList
+                      tasks={todos}
+                      status={"Todo"}
+                      updateTaskFun={updateTaskFun}
+                    />
+                  </div>
+                  {/* task modal end */}
                 </div>
-                {/* task modal end */}
               </div>
-            </div>
-            {/* in-progress card end*/}
+              {/* to do card end */}
 
-            {/* completed card */}
-            <div className=" bg-gray-900 flex-col items-center  rounded ">
-              <div className="m-3">
-                <p className="m-2 text-center text-lg mt-1 text-white ">
-                  Completed
-                </p>
-                <div>
-                  <TaskAreaModal status={"Completed"} addTask={addTask} />
-
-                  <TaskList
-                    tasks={completed}
-                    status={"Completed"}
-                    updateTaskFun={updateTaskFun}
-                  />
+              {/* in-progress card */}
+              <div className=" bg-gray-900 flex-col items-center  rounded ">
+                <div className="m-3">
+                  <p className="m-2 text-center text-lg mt-1 text-white ">
+                    In-Progress
+                  </p>
+                  <div>
+                    <TaskAreaModal status={"In Progress"} addTask={addTask} />
+                    <TaskList
+                      tasks={inprogress}
+                      status={"In Progress"}
+                      updateTaskFun={updateTaskFun}
+                    />
+                  </div>
+                  {/* task modal end */}
                 </div>
-                {/* task modal end */}
               </div>
+              {/* in-progress card end*/}
+
+              {/* completed card */}
+              <div className=" bg-gray-900 flex-col items-center  rounded ">
+                <div className="m-3">
+                  <p className="m-2 text-center text-lg mt-1 text-white ">
+                    Completed
+                  </p>
+                  <div>
+                    <TaskAreaModal status={"Completed"} addTask={addTask} />
+
+                    <TaskList
+                      tasks={completed}
+                      status={"Completed"}
+                      updateTaskFun={updateTaskFun}
+                    />
+                  </div>
+                  {/* task modal end */}
+                </div>
+              </div>
+              {/* completed card end*/}
             </div>
-            {/* completed card end*/}
-          </div>
+          )}
         </div>
       </div>
+
+      {/* task cards */}
+
       {/* task cards ends here */}
     </div>
   );
