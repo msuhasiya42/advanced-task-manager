1351a40da4faa60a98c6f91cbd5fda2a972b4bf0
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-06-22 19:37:45 +0530

    side bar tags are visible directly

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -226,8 +226,6 @@ const SideBar = () => {
               <button
                 type="button"
                 className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                aria-controls="dropdown-example"
-                data-collapse-toggle="dropdown-example"
               >
                 <svg
                   fill="none"
@@ -244,27 +242,11 @@ const SideBar = () => {
                     d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5"
                   ></path>
                 </svg>
-                <span
-                  className="flex-1 ml-3 text-left whitespace-nowrap"
-                  // sidebar-toggle-item
-                >
+                <span className="flex-1 ml-3 text-left whitespace-nowrap">
                   Tags
                 </span>
-                <svg
-                  // sidebar-toggle-item
-                  className="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
               </button>
-              <ul id="dropdown-example" className="hidden py-2 space-y-2">
+              <ul className=" py-2 space-y-2">
                 {tags.map((tag, index) => (
                   <li key={index}>
                     <a
