6490b84972abb241a59887b8d7e2992b56869d34
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-07-09 10:46:51 +0530

    removed fronend from backend repo

--- frontend/src/config.js
+++
@@ -1,3 +0,0 @@
-const API_URL = "http://localhost:5000"; // Replace with your backend URL
-
-export default API_URL;
--- frontend/src/utils/functions.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from "react-router-dom";
-
-export const Logout = () => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("userId");
-  redirect("/login");
-};
