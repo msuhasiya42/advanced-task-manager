37621e676a94da83f34b0a1fb58d3b580b09b125
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-10-28 11:18:41 +0530

    Refactor task manager

--- frontend/src/components/Task/TaskManager.tsx
+++ frontend/src/components/Task/TaskManager.tsx
@@ -6,87 +6,74 @@ import useTaskStore from "../../Zustand/taskStore";
 import useAuthStore from "../../Zustand/authStore";
 import { taskSchema } from "../../zodSpecs/task";
 import { TaskType } from "./Types/types";
+
 const TaskManager = () => {
   const [loading, setLoading] = useState(false);
-
-  // using zustand store
   const { user } = useAuthStore();
+
   const setAllTasks = useTaskStore((state) => state.setAllTasks);
   const setOriginalTasks = useTaskStore((state) => state.setOriginalTasks);
   const copyTasks = useTaskStore((state) => state.copyTasks);
   const copiedTasks = useTaskStore((state) => state.copiedTasks);
 
-  useEffect(() => {
-    setLoading(true);
+  const filterTasksByStatus = (tasks: TaskType[], status: string) => {
+    return tasks.filter((task) => task.status === status);
+  };
 
-    // fetching all tasks
-    const fetchTaskFun = async (user: string) => {
-      fetchTask(user)
-        .then((response) => {
-          const fetchedTasks = response.data.tasks;
+  const fetchAndProcessTasks = async (userId: string) => {
+    try {
+      setLoading(true);
+      const response = await fetchTask(userId);
+      const fetchedTasks = response.data.tasks;
 
-          // zod validation
-          const validatedTasks = fetchedTasks.map((task: TaskType) => {
-            try {
-              return taskSchema.parse(task);
-            } catch (error: any) {
-              console.error(`Invalid task: ${error.message}`);
-              return null;
-            }
-          });
+      const validatedTasks = fetchedTasks.map((task: TaskType) => {
+        try {
+          return taskSchema.parse(task);
+        } catch (error: any) {
+          console.error(`Invalid task: ${error.message}`);
+          return null;
+        }
+      });
 
-          setAllTasks(validatedTasks);
+      setAllTasks(validatedTasks);
 
-          const todos = validatedTasks.filter(
-            (task: TaskType) => task.status === "todo"
-          );
-          const inprogress = validatedTasks.filter(
-            (task: TaskType) => task.status === "inProgress"
-          );
-          const completed = validatedTasks.filter(
-            (task: TaskType) => task.status === "completed"
-          );
+      const todos = filterTasksByStatus(validatedTasks, "todo");
+      const inprogress = filterTasksByStatus(validatedTasks, "inProgress");
+      const completed = filterTasksByStatus(validatedTasks, "completed");
 
-          // Storing data to main store
-          setOriginalTasks("todo", todos); // Set the fetched tasks in the "todo" category
-          setOriginalTasks("inProgress", inprogress); // Set the fetched tasks in the "inProgress" category
-          setOriginalTasks("completed", completed);
+      setOriginalTasks("todo", todos);
+      setOriginalTasks("inProgress", inprogress);
+      setOriginalTasks("completed", completed);
 
-          // copying orig store into copied store
-          copyTasks();
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-
-    if (user?.userId) {
-      fetchTaskFun(user.userId);
+      copyTasks();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (user?.userId) {
+      fetchAndProcessTasks(user.userId);
+    }
   }, [user, setOriginalTasks]);
 
   return (
-    <div>
-      <div className=" border-gray-900 border-dashed rounded-lg ">
-        {loading ? (
-          <LoadingPage />
-        ) : (
-          <>
-            <div className=" bg-gray-900 ml-1 grow h-full rounded ">
-              <div className="m-1">
-                <div className="">
-                  <TaskList
-                    todo={copiedTasks.todo}
-                    inProgress={copiedTasks.inProgress}
-                    completed={copiedTasks.completed}
-                  />
-                </div>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
+    <div className="border-gray-900 border-dashed rounded-lg">
+      {loading ? (
+        <LoadingPage />
+      ) : (
+        <div className="bg-gray-900 ml-1 grow h-full rounded">
+          <div className="m-1">
+            <TaskList
+              todo={copiedTasks.todo}
+              inProgress={copiedTasks.inProgress}
+              completed={copiedTasks.completed}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
