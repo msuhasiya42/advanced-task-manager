56bd49ecc90aadb5bbc52ce7d6f238930a237460
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2023-10-28 17:46:55 +0530

    Updated about us

--- frontend/src/components/AboutUs/AboutUs.tsx
+++ frontend/src/components/AboutUs/AboutUs.tsx
@@ -1,26 +1,15 @@
 import React from "react";
 import Header from "../Header/Header";
 
-const AboutUs = () => {
-  return (
-    <div>
-      <Header />
-      <h1 className="font-bold text-7xl text-center">
+const AboutUs = () => (
+  <div className="h-screen flex flex-col">
+    <Header />
+    <main className="flex-grow flex items-center justify-center">
+      <span className="font-light text-white text-5xl text-center">
         Organize your life, Increase your productivity
-      </h1>
-      <h3 className="font-bold text-3xl">WHO ARE WE?</h3>
-      <div>
-        One year back, when two people join the same company and later became
-        good friends. They started working together,chit chatting, sharing their
-        feeling. They wanted to progress in life together,and wanted to help
-        each other so they built this project as their first step to success. We
-        started woring on this new Advanced Task manager app which will help us
-        understand more about MERN technology specially Mongodb, Express, and
-        Node.js.
-      </div>
-      {/* <Footer /> */}
-    </div>
-  );
-};
+      </span>
+    </main>
+  </div>
+);
 
 export default AboutUs;
