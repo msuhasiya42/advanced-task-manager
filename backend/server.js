fae796decae854fcbcf8c5f3ad0c21f63265e7e1
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2024-01-07 11:45:46 +0530

    rename to URI

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 
 // mongodb connections
-const DB = process.env.MONGODB_URL;
+const DB = process.env.MONGODB_URI;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
