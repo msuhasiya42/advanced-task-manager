fa95ce3018accc5c70d2c92eff3c10f65ce90c6b
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2024-01-07 13:54:20 +0530

    server.js code refactor

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-// This line imports the body-parser middleware module. It allows parsing of incoming
-// request bodies in different formats, such as JSON or URL-encoded.
 const bodyParser = require("body-parser");
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-// This line registers the built-in middleware provided by Express to parse JSON request bodies.
-// It enables the server to handle requests with JSON payloads.
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// This line registers the cors middlewarre, enabling Cross-Origin Resource Sharing.
-// It allows requests from different origins to access this server's resources.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,21 +18,17 @@ const DB = process.env.MONGODB_URI;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    // useCreateIndex: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
+    useUnifiedTopology: true
   })
   .then(() => {
-    console.log("connection successfull");
+    console.log("Connection successful");
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log(err + "connection failed");
+    console.log(err + " Connection failed");
   });
 
 const routes = require("./Routes");
 app.use("/", routes);
-
-// Start the server
