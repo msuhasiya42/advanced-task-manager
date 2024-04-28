8357b0ff7904edeb3f84a90ebf7310e7de2df3d4
msuhasyia42 <mayursuhasiya136@gmail.com>
2024-04-28 21:52:49 +0530

    lading page changes

--- frontend/src/components/NavBar/NavBarHomePage.tsx
+++ frontend/src/components/NavBar/NavBarHomePage.tsx
@@ -11,34 +11,13 @@ const NavBar = () => {
   return (
     <div className="w-full ">
       <nav className="bg-white shadow-md border-gray-200 dark:bg-gray-900">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex flex-wrap items-center justify-center sm:justify-between mx-auto p-4">
           <a href="/" className="flex items-center">
             <span className="self-center text-2xl font-semibold bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent whitespace-nowrap">
               Task-Manager
             </span>
           </a>
-          <button
-            data-collapse-toggle="navbar-dropdown"
-            type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-purple-600 transition-all duration-300"
-            aria-controls="navbar-dropdown"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
+
           <div
             className="hidden w-full md:block md:w-auto"
             id="navbar-dropdown"
