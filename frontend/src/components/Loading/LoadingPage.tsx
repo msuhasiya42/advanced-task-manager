1f2cb0f29d55e89c4a543d7caa2864deb93ce687
Mayur Suhasiya <mayursuhasiya136@gmail.com>
2024-05-18 19:09:38 +0530

    loading fix

--- frontend/src/components/Loading/LoadingPage.tsx
+++ frontend/src/components/Loading/LoadingPage.tsx
@@ -3,13 +3,9 @@ import React from "react";
 
 const LoadingPage = () => {
   return (
-    <div>
-      <div className="flex min-h-screen items-center justify-center w-full h-full  border-gray-200  bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-        <div role="status">
-          <LoadingOutlined />
-          <span className="sr-only">Loading...</span>
-        </div>
-      </div>
+    <div role="status">
+      <LoadingOutlined />
+      <span className="sr-only">Loading...</span>
     </div>
   );
 };
